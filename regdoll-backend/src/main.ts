c8d1e9a75bc6c03d3e9c14231673a7f0566b133f
lgldlk <1851293758@qq.com>
2021-04-30 18:45:36 +0800

    设置访问频率

--- regdoll-backend/src/main.ts
+++ regdoll-backend/src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import * as rateLimit from 'express-rate-limit';
 import { AppModule } from './app.module';
 import * as helmet from 'helmet';
 import { EnvSwaggerOptions } from './config/swagger.config';
@@ -7,10 +8,19 @@ import { ConfigService } from '@nestjs/config';
 import { ServeOptions } from './config/serveConfig';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+  });
   const configService = app.get(ConfigService);
   const swaggerOptions = configService.get<EnvSwaggerOptions>(
     'EnvSwaggerOptions',
+  );
+  // 访问频率限制
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000, // 15分钟
+      max: 1000, // 限制15分钟内最多只能访问1000次
+    }),
   );
   // web 漏洞
   app.use(helmet());
