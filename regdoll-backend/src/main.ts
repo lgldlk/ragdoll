4448de5c5c4dda87e59f7505552b7a726963f421
lgldlk <1851293758@qq.com>
2021-04-30 16:08:17 +0800

    防范部分漏洞

--- regdoll-backend/src/main.ts
+++ regdoll-backend/src/main.ts
@@ -1,15 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { EnvSwaggerOptions } from './config/env/swagger.config';
+import * as helmet from 'helmet';
+import { EnvSwaggerOptions } from './config/swagger.config';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ConfigService } from '@nestjs/config';
-import { ServeOptions } from './config/env/serveConfig';
+import { ServeOptions } from './config/serveConfig';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const swaggerOptions = configService.get<EnvSwaggerOptions>(
     'EnvSwaggerOptions',
   );
+  // web 漏洞
+  app.use(helmet());
   const serveOptions = configService.get<ServeOptions>('serveOptions');
   const options = new DocumentBuilder()
     .setTitle(swaggerOptions.title)
