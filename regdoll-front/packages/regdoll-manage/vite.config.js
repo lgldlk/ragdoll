63d7cb0caf41990cc8027e5b93a76696207e29cf
lgldlk <1851293758@qq.com>
2021-05-04 20:20:53 +0800

    配置相对引入

---
+++ regdoll-front/packages/regdoll-manage/vite.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+module.exports = {
+    cssPreprocessOptions: {
+        scss: {},
+    },
+    alias: {
+        // 键必须以斜线开始和结束
+        '/@/': path.resolve(__dirname, './src'),
+        // '/@components/': path.resolve(__dirname, './src/components')
+    },
+};
