0afa7367abbccad0b138cbd7d7b7aad23d3c1b8f
lgldlk <1851293758@qq.com>
2021-05-10 22:21:21 +0800

    📦 添加根据电子数获取电子分布数组函数

--- regdoll-front/packages/regdoll-manage/src/threeScene/Atom.ts
+++ regdoll-front/packages/regdoll-manage/src/threeScene/Atom.ts
@@ -1,12 +1,22 @@
+/*
+ * @Descripttion:
+ * @Author: lgldlk
+ * @Date: 2021-05-02 21:54:10
+ * @Editors: lgldlk
+ * @LastEditTime: 2021-05-10 21:42:26
+ */
 import * as THREE from 'three';
+import { Object3D } from 'three';
 interface AtomTrack {
   radius: Number;
   eleNumber: Number;
 }
-export default class Atom {
+export default class Atom extends Object3D {
   constructor(
     public quality: Number = 0,
     public electronicNumber: Number = 0,
     public tracks: Array<AtomTrack> = [],
-  ) {}
+  ) {
+    super();
+  }
 }
--- regdoll-front/packages/regdoll-manage/src/threeScene/regDollHelper.ts
+++ regdoll-front/packages/regdoll-manage/src/threeScene/regDollHelper.ts
@@ -4,7 +4,13 @@ import * as THREE from 'three';
  * @Author: lgldlk
  * @Date: 2021-05-10 21:00:12
  * @Editors: lgldlk
- * @LastEditTime: 2021-05-10 21:04:37
+ * @LastEditTime: 2021-05-10 22:20:01
+ */
+/**
+ * 获取帮助表格
+ * @param size 设置大小
+ * @param divisions 设置有几个格子
+ * @returns  THREE.GridHelper实例
  */
 export function getGridHelper(size: number, divisions: number) {
   let gridHelp = new THREE.GridHelper(size, divisions);
@@ -12,4 +18,26 @@ export function getGridHelper(size: number, divisions: number) {
   gridHelp.material instanceof THREE.Material && (gridHelp.material.opacity = 0.25);
   gridHelp.material instanceof THREE.Material && (gridHelp.material.transparent = true);
   return gridHelp;
+}
+
+/**
+ * 根据电子数获取电子分布数组
+ * @param eleNum 电子数
+ * @returns 电子数分布数组
+ */
+export function getTrackArr(eleNum: number): Array<number> {
+  if (eleNum <= 2) {
+    return [eleNum];
+  } else if (eleNum <= 10) {
+    return [2, (eleNum - 2) % 8];
+  } else if (eleNum <= 18) {
+    return [2, 8, (eleNum - 10) % 8];
+  } else if (eleNum <= 36) {
+    return [2, 8, 18, (eleNum - 28) % 8];
+  } else if (eleNum <= 52) {
+    return [2, 8, 18, 18, (eleNum - 44) % 8];
+  } else if (eleNum <= 86) {
+    return [2, 8, 18, 32, 18, (eleNum - 78) % 8];
+  }
+  return [];
 }
