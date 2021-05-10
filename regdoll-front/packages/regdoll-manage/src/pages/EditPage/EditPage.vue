1b7c6a15b3c218de31c43a2d857a126f6840810e
lgldlk <1851293758@qq.com>
2021-05-10 21:23:16 +0800

    💄 更新样式

--- regdoll-front/packages/regdoll-manage/src/pages/EditPage/EditPage.vue
+++ regdoll-front/packages/regdoll-manage/src/pages/EditPage/EditPage.vue
@@ -1,3 +1,10 @@
+<!--
+ * @Descripttion: 
+ * @Author: lgldlk
+ * @Date: 2021-05-03 19:54:41
+ * @Editors: lgldlk
+ * @LastEditTime: 2021-05-10 20:59:22
+-->
 <template>
   <div class="edit-page">
     <leaf-column class="leaf-column" />
@@ -7,15 +14,23 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, onMounted } from 'vue';
 import leafColumn from './components/LeafColumn.vue';
 import rightColumn from './components/RightColumn.vue';
 import centerScene from './components/CenterScene.vue';
-import { proParts } from './data';
+import { proParts } from './data/parts';
+import { initScene } from './data/mainScene';
+
 export default defineComponent({
   components: { leafColumn, rightColumn, centerScene },
   setup() {
     proParts();
+    onMounted(() => {
+      let tmpScene = document.getElementById('mainScene');
+      if (tmpScene) {
+        initScene(tmpScene);
+      }
+    });
   },
 });
 </script>
--- regdoll-front/packages/regdoll-manage/src/pages/EditPage/components/CenterScene.vue
+++ regdoll-front/packages/regdoll-manage/src/pages/EditPage/components/CenterScene.vue
@@ -1,5 +1,12 @@
+<!--
+ * @Descripttion: 
+ * @Author: lgldlk
+ * @Date: 2021-05-03 21:07:32
+ * @Editors: lgldlk
+ * @LastEditTime: 2021-05-10 16:56:00
+-->
 <template>
-  <div>1346</div>
+  <div id="mainScene"></div>
 </template>
 
 <script lang="ts">
