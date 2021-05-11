45b4f99339067d8eb442aca2748714592eb88772
lgldlk <1851293758@qq.com>
2021-05-11 22:27:38 +0800

    🤔 全局设定更新

--- regdoll-front/packages/regdoll-manage/src/config/AtomModelConfig.ts
+++ regdoll-front/packages/regdoll-manage/src/config/AtomModelConfig.ts
@@ -1,21 +1,44 @@
+/*
+ * @Description:
+ * @Author: lgldlk
+ * @Date: 2021-05-02 21:54:09
+ * @Editors: lgldlk
+ * @LastEditTime: 2021-05-11 21:54:16
+ */
+
+import * as THREE from 'three';
+
 interface DefaultSphereConfigType {
-  widthSegments: Number; //默认宽段数
-  heightSegments: Number; //默认高段数
+  //球类的默认设置
+
+  widthSegments: number; //默认宽段数
+  heightSegments: number; //默认高段数
+  shininess: number; //默认反光指数
 }
+
 interface DefaultTorusConfigType {
-  tube: Number;
-  radialSegments: Number;
-  tubularSegments: Number; // 圆环的默认环段数
+  //圆环的默认设置
+  tube: number;
+  radialSegments: number;
+  tubularSegments: number; // 圆环的默认环段数
 }
+
 interface DefaultEleConfigType extends DefaultSphereConfigType {
-  radius: Number;
+  // 默认电子设置
+  color: THREE.Color;
+  radius: number;
+}
+interface DefaultNucleusConfig extends DefaultSphereConfigType {
+  // 默认原子核设置
+  color: THREE.Color;
+  baseRadius: number; //默认的原子核大小基数
 }
 export default class AtomModelConfig {
-  static baseAtomRadius: Number = 1; //原子半径的基数
-  static shininess: Number = 100; //反光材质亮度
+  static shininess: number = 100; //反光材质亮度
   static defaultSphere: DefaultSphereConfigType = {
     widthSegments: 32,
     heightSegments: 32,
+    shininess: 100,
   };
   static defaultTorus: DefaultTorusConfigType = {
     tube: 0.05,
@@ -24,6 +47,12 @@ export default class AtomModelConfig {
   };
   static defaultEleConfig: DefaultEleConfigType = {
     ...AtomModelConfig.defaultSphere,
+    color: new THREE.Color(0xffff00),
     radius: 0.5,
+  };
+  static defaultNucleusConfig: DefaultNucleusConfig = {
+    ...AtomModelConfig.defaultSphere,
+    color: new THREE.Color(0xee0000),
+    baseRadius: 1,
   };
 }
--- regdoll-front/packages/regdoll-manage/src/pages/EditPage/components/Parts.vue
+++ regdoll-front/packages/regdoll-manage/src/pages/EditPage/components/Parts.vue
@@ -1,3 +1,10 @@
+<!--
+ * @Descripttion: 
+ * @Author: lgldlk
+ * @Date: 2021-05-04 21:10:34
+ * @Editors: lgldlk
+ * @LastEditTime: 2021-05-11 21:00:24
+-->
 <template>
   <div class="parts">
     <div class="part" v-for="(item, i) in parts" :key="i">
@@ -9,7 +16,7 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import { getParts } from '../data/index';
+import { getParts } from '../data/parts';
 
 export default defineComponent({
   setup() {
