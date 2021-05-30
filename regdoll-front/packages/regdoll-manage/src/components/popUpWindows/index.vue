9beabfd124ce8cca9c07d2ce930806d78fedc3f1
lgldlk <1851293758@qq.com>
2021-05-30 20:48:46 +0800

    弹窗组件

---
+++ regdoll-front/packages/regdoll-manage/src/components/popUpWindows/index.vue
@@ -0,0 +1,70 @@
+<!--
+ * @Descripttion: 
+ * @Author: lgldlk
+ * @Date: 2021-05-29 21:05:26
+ * @Editors: lgldlk
+ * @LastEditTime: 2021-05-30 20:48:09
+-->
+<template>
+  <teleport to="body">
+    <div class="pop-bg" @click="bgClick">
+      <div class="center-slot">
+        <div class="slot-header">
+          <div class="header-title">{{ windowTitle }}</div>
+
+          <svg
+            t="1622298296948"
+            class="forkIcon"
+            viewBox="0 0 1024 1024"
+            @click.stop="forkClick"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M77.550933 29.2864l917.162667 917.162667-48.264533 48.264533L29.2864 77.550933z"
+              fill="#2c2c2c"
+            ></path>
+            <path
+              d="M946.449067 29.2864L29.2864 946.449067l48.264533 48.264533 917.162667-917.162667z"
+              fill="#2c2c2c"
+            ></path>
+          </svg>
+        </div>
+        <slot> </slot>
+      </div>
+    </div>
+  </teleport>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "",
+  props: {
+    windowTitle: String,
+  },
+  emits: ["bgClick", "forkClick"],
+  setup(props, ctx) {
+    const bgClick = () => {
+      ctx.emit("bgClick");
+    };
+    const forkClick = () => {
+      ctx.emit("forkClick");
+    };
+    const windowTitle = props.windowTitle;
+    return { bgClick, windowTitle, forkClick };
+  },
+});
+</script>
+<style scoped lang="scss">
+@import "./index.scss";
+.pop-bg {
+  .center-slot {
+    .slot-header {
+      .header-title {
+      }
+      .header-fork {
+      }
+    }
+  }
+}
+</style>
