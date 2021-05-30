d6d8c4b12d1457b2d438c5f8aedf2c8bbc9f7ae0
lgldlk <1851293758@qq.com>
2021-05-30 20:48:59 +0800

    url配置

---
+++ regdoll-front/packages/regdoll-manage/src/config/Url.ts
@@ -0,0 +1,8 @@
+/*
+ * @Descripttion:
+ * @Author: lgldlk
+ * @Date: 2021-05-28 22:30:17
+ * @Editors: lgldlk
+ * @LastEditTime: 2021-05-29 15:00:46
+ */
+export const BaseUrl = 'http://localhost:9999/api';
