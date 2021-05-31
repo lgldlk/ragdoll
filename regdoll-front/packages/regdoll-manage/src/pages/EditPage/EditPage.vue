187d777d1857d07b97337319c893361a97b9b799
lgldlk <1851293758@qq.com>
2021-05-31 22:26:28 +0800

    增加原子选则弹窗

--- regdoll-front/packages/regdoll-manage/src/pages/EditPage/EditPage.vue
+++ regdoll-front/packages/regdoll-manage/src/pages/EditPage/EditPage.vue
@@ -3,37 +3,48 @@
  * @Author: lgldlk
  * @Date: 2021-05-03 19:54:41
  * @Editors: lgldlk
- * @LastEditTime: 2021-05-10 20:59:22
+ * @LastEditTime: 2021-05-31 21:58:25
 -->
 <template>
   <div class="edit-page">
     <leaf-column class="leaf-column" />
     <center-scene class="center-scene" />
     <right-column class="right-column" />
+    <popWindow
+      v-if="showAtomChooseWindow"
+      :windowTitle="'原子选择'"
+      @bgClick="closeAtomChooseWindow"
+      @forkClick="closeAtomChooseWindow"
+    ></popWindow>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted } from 'vue';
-import leafColumn from './components/LeafColumn.vue';
-import rightColumn from './components/RightColumn.vue';
-import centerScene from './components/CenterScene.vue';
-import { proParts } from './data/parts';
-import { initScene } from './data/mainScene';
-
+import { defineComponent, onMounted, ref, SetupContext } from "vue";
+import leafColumn from "./components/LeafColumn.vue";
+import rightColumn from "./components/RightColumn.vue";
+import centerScene from "./components/CenterScene.vue";
+import { proParts } from "./data/parts";
+import { initScene } from "./data/mainScene";
+import AtomChooseWindow from "./data/AtomChooseWindow";
+import popWindow from "/@/components/popUpWindows/index.vue";
 export default defineComponent({
-  components: { leafColumn, rightColumn, centerScene },
-  setup() {
+  components: { leafColumn, popWindow, rightColumn, centerScene },
+  setup(props, content: SetupContext) {
     proParts();
     onMounted(() => {
-      let tmpScene = document.getElementById('mainScene');
+      let tmpScene = document.getElementById("mainScene");
       if (tmpScene) {
         initScene(tmpScene);
       }
     });
+    const AtomChooseWindowModule = AtomChooseWindow();
+    return {
+      ...AtomChooseWindowModule,
+    };
   },
 });
 </script>
 <style scoped lang="scss">
-@import './EditPage.scss';
+@import "./EditPage.scss";
 </style>
