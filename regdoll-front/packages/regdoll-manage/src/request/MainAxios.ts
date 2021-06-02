b6fa6e663a762fe9dbd7222482850a2274aaa648
lgldlk <1851293758@qq.com>
2021-06-02 21:59:41 +0800

    增加请求失败提示，以及response反馈类型

--- regdoll-front/packages/regdoll-manage/src/request/MainAxios.ts
+++ regdoll-front/packages/regdoll-manage/src/request/MainAxios.ts
@@ -3,24 +3,27 @@
  * @Author: lgldlk
  * @Date: 2021-05-28 22:28:09
  * @Editors: lgldlk
- * @LastEditTime: 2021-05-29 14:36:33
+ * @LastEditTime: 2021-06-02 21:58:25
  */
-import Request from './Request';
-import { BaseUrl } from '/@/config/Url';
-
+import Request from "./Request";
+import { BaseUrl, reqErrorMsg } from "../config/RequestConfig";
+import { ElMessage } from "element-plus";
 let request = new Request();
 request.init({
   baseUrl: BaseUrl,
-  response: (res: response) => {
+  response: (res: response): Promise<responseData> => {
     // 请求后置拦截器
-    if (res.data.code == '200') {
+    if (res.data.code == "200") {
       let data: responseData = res.data;
       return Promise.resolve(data);
-    } else if (res.data.code == '400') {
+    } else if (res.data.code == "400") {
+      ElMessage.error(res.data.message || reqErrorMsg);
       return Promise.reject(res.data.message);
-    } else if (res.data.code == '500') {
+    } else if (res.data.code == "500") {
+      ElMessage.error(res.data.message || reqErrorMsg);
       return Promise.reject(res.data.message);
     } else {
+      ElMessage.error(res.data.message || reqErrorMsg);
       console.log(res);
       return Promise.reject(res.data.message);
     }
