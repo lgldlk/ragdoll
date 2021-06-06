946754a0b3a8de7b596de33eb4931ecd4d4e44ba
lgldlk <1851293758@qq.com>
2021-06-06 22:41:47 +0800

    使用elementui自定义主题

--- regdoll-front/packages/regdoll-manage/src/main.ts
+++ regdoll-front/packages/regdoll-manage/src/main.ts
@@ -3,6 +3,13 @@
  * @Author: lgldlk
  * @Date: 2021-05-04 20:21:16
  * @Editors: lgldlk
+ * @LastEditTime: 2021-06-06 22:41:03
+ */
+/*
+ * @Descripttion:
+ * @Author: lgldlk
+ * @Date: 2021-05-04 20:21:16
+ * @Editors: lgldlk
  * @LastEditTime: 2021-06-03 19:51:27
  */
 import { createApp } from "vue";
@@ -10,6 +17,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 
 import router from "./router";
+import "./assets/css/theme/index.css";
 import ElementPlus from "element-plus";
 import store from "./store/index";
 const app = createApp(App as any);
