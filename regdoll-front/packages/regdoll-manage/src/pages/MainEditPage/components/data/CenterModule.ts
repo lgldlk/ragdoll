322504546699d019f37d765ca349f3aa6a21ba43
lgldlk <1851293758@qq.com>
2021-07-03 22:17:27 +0800

    基础圆形图形

--- regdoll-front/packages/regdoll-manage/src/pages/MainEditPage/components/data/CenterModule.ts
+++ regdoll-front/packages/regdoll-manage/src/pages/MainEditPage/components/data/CenterModule.ts
@@ -1,27 +1,36 @@
 import { SCENE_MODULE_COMMIT_PREFIX } from './../../../../store/index';
-import  BoxModel  from '/@/threeScene/BaseModel/BoxModel';
+import BoxModel from '/@/threeScene/BaseModel/BoxModel';
 /*
  * @Descripttion: 
  * @Author: lgldlk
  * @Date: 2021-06-10 21:07:48
  * @Editors: lgldlk
- * @LastEditTime: 2021-06-10 22:14:20
+ * @LastEditTime: 2021-07-02 22:29:30
  */
 
 import { Store } from 'vuex';
 import store, { RootState } from '/@/store';
 import { ADD_OBJECT } from '/@/store/Scene/mutation-types';
+import CircleModel from '/@/threeScene/BaseModel/CircleModel';
 
-const baseModRefer:any = {
-  "BoxModel":BoxModel
+
+export const DARG_KEY = {
+  boxModel: "BoxModel",
+  circleModel: "CircleModel",
+
 }
-, mainSceneDrop = (e: Event) => {
-  
-  store.commit(SCENE_MODULE_COMMIT_PREFIX+ADD_OBJECT, [new baseModRefer[e.dataTransfer.getData("dargPart")](),e])
-  e.preventDefault();
-}, mainSceneDragover = (e: Event) => {
-  e.preventDefault();
+const baseModRefer: any = {
+  [DARG_KEY.boxModel]: BoxModel,
+  [DARG_KEY.circleModel]: CircleModel
 }
+  , mainSceneDrop = (e: Event) => {
+    if (e.dataTransfer.getData("dargPart") != undefined) {
+      store.commit(SCENE_MODULE_COMMIT_PREFIX + ADD_OBJECT, [new baseModRefer[e.dataTransfer.getData("dargPart")](), e])
+      e.preventDefault();
+    }
+  }, mainSceneDragover = (e: Event) => {
+    e.preventDefault();
+  }
 export default function centerMod(storeIn: Store<RootState>) {
   return {
     mainSceneDrop
