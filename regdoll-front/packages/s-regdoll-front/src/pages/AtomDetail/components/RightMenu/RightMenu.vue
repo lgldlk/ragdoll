70a3002c8464b42780d818b64ecad615ec990fdc
lgldlk <1851293758@qq.com>
2021-07-06 11:21:29 +0800

    右侧菜单添加原子弹窗

--- regdoll-front/packages/s-regdoll-front/src/pages/AtomDetail/components/RightMenu/RightMenu.vue
+++ regdoll-front/packages/s-regdoll-front/src/pages/AtomDetail/components/RightMenu/RightMenu.vue
@@ -3,12 +3,12 @@
  * @Author: lgldlk
  * @Date: 2021-07-05 13:49:55
  * @Editors: lgldlk
- * @LastEditTime: 2021-07-06 10:37:09
+ * @LastEditTime: 2021-07-06 10:52:36
 -->
 <template>
   <div class="right_menu">
     <div class="choose_menu">
-      
+
     </div>
     <div class="tool_menu">
       <div class="tools_container">
@@ -17,8 +17,10 @@
             <div class="tool_options">
               <div class="tool_option"
                    v-for="(item,i) in toolsLIst"
-                   :key="i">
-                <div class="icon_con" v-html="item.image"></div>
+                   :key="i"
+                   @click="item.clickFunc">
+                <div class="icon_con"
+                     v-html="item.image"></div>
                 <div class="tool_title">{{item.title}}</div>
               </div>
             </div>
@@ -27,15 +29,43 @@
 
       </div>
     </div>
+    <popWindow v-if="showAtomChooseWindow"
+               windowTitle="原子选择"
+               @forkClick="closeAtomChooseWindow">
+      <div>
+        <el-select v-model="chooseAtomEnName"
+                   placeholder="请选择要添加的原子">
+          <el-option v-for="item in atomArray"
+                     :key="item.ch_name"
+                     :label="item.ch_name"
+                     :value="item.en_name">
+          </el-option>
+        </el-select>
+      </div>
+      <template #footer>
+        <span class="popWindow_footer">
+          <el-button size="mini"
+                     :round="true"
+                     @click="closeAtomChooseWindow">取 消</el-button>
+          <el-button size="mini"
+                     type="primary"
+                     round="round"
+                     @click="affirmChooseAtom">确 定</el-button>
+        </span>
+      </template>
+    </popWindow>
+
   </div>
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
 import rightToolModule from "./Composition/RightTools";
 import { useStore } from "vuex";
+import popWindow from "/@/components/popUpWindows/index.vue";
 import { log } from "util";
 export default defineComponent({
 	name: "",
+	components: { popWindow },
 	setup() {
 		const store = useStore(),
 			rightTool = rightToolModule(store);
