1dde4d8f675cbeb8d1d69f9ac07640a64b0386e9
lgldlk <1851293758@qq.com>
2021-07-16 14:23:49 +0800

    请求的包装

---
+++ regdoll-front/packages/s-regdoll-manage/src/request/MainAxios.ts
@@ -0,0 +1,33 @@
+/*
+ * @Descripttion:
+ * @Author: lgldlk
+ * @Date: 2021-05-28 22:28:09
+ * @Editors: lgldlk
+ * @LastEditTime: 2021-07-13 14:31:55
+ */
+import Request from "./Request";
+import { BaseUrl, reqErrorMsg } from "../config/RequestConfig";
+import { ElMessage } from "element-plus";
+let request = new Request();
+request.init({
+  baseUrl: BaseUrl,
+  response: (res: response): Promise<responseData> => {
+    // 请求后置拦截器
+    if (res.data.code == "200") {
+      let data: responseData = res.data;
+      return Promise.resolve(data);
+    } else if (res.data.code == "400") {
+      ElMessage.error(res.data.message || reqErrorMsg);
+      return Promise.reject(res.data.message);
+    } else if (res.data.code == "500") {
+      ElMessage.error(res.data.message || reqErrorMsg);
+      return Promise.reject(res.data.message);
+    } else {
+      ElMessage.error(res.data.message || reqErrorMsg);
+      console.log(res);
+      return Promise.reject(res.data.message);
+    }
+  },
+});
+
+export default request;
