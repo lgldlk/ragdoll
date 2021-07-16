96a48085c0314712d1cec9e2feb4f2193e5277a6
lgldlk <1851293758@qq.com>
2021-07-16 14:29:26 +0800

    分子接口

--- regdoll-backend/src/system/Molecule/molecule.controller.ts
+++ regdoll-backend/src/system/Molecule/molecule.controller.ts
@@ -3,13 +3,13 @@
  * @Author: lgldlk
  * @Date: 2021-07-12 08:56:28
  * @Editors: lgldlk
- * @LastEditTime: 2021-07-13 14:58:34
+ * @LastEditTime: 2021-07-16 14:21:25
  */
 /*
 https://docs.nestjs.com/controllers#controllers
 */
 
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { ConstituentAtoms } from 'src/database/entitys/ConstituentAtoms.entity';
 import { Molecule } from 'src/database/entitys/Molecule.entity';
 import { MoleculeService } from './molecule.service';
@@ -20,10 +20,20 @@ export class MoleculeController {
 
   }
   @Post("addMolecule")
-  addMolecule(@Body("molecule") molecule: Molecule) {
-    this.moleculeService.addMolecule(molecule)
-    return { code: '200', molecule };
-
+  async addMolecule(@Body("molecule") molecule: Molecule) {
+    // console.log(molecule);
+    return await this.moleculeService.addMolecule(molecule)
   }
-
+  @Get("allMolecule")
+  async allMolecule() {
+    return await this.moleculeService.allMolecule();
+  }
+  @Post("deleteMolecule")
+  async deleteMolecule(@Body() body) {
+    return await this.moleculeService.deleteMolecule(body.id);
+  }
+  @Post("updateMoleculeValence")
+  async updateMoleculeValence(@Body("molecule") molecule: Molecule) {
+    return await this.moleculeService.updateMoleculeValence(molecule)
+  }
 }
--- regdoll-backend/src/system/Molecule/molecule.service.ts
+++ regdoll-backend/src/system/Molecule/molecule.service.ts
@@ -3,7 +3,7 @@
  * @Author: lgldlk
  * @Date: 2021-07-12 08:56:44
  * @Editors: lgldlk
- * @LastEditTime: 2021-07-13 14:55:43
+ * @LastEditTime: 2021-07-16 11:14:58
  */
 import { Molecule } from 'src/database/entitys/Molecule.entity';
 /*
@@ -17,17 +17,48 @@ import { ConstituentAtoms } from 'src/database/entitys/ConstituentAtoms.entity';
 
 @Injectable()
 export class MoleculeService {
+  async updateMoleculeValence(molecule: Molecule) {
+    await this.moleculeRepo.save(molecule);
+    for (let tmp of molecule.atomDatas) {
+      await this.constituentAtomRepo.update({ id: tmp.id }, { valence: tmp.valence })
+    }
+    return {
+      code: "200",
+      message: "更新成功"
+    }
+  }
+  async deleteMolecule(id: any) {
+    await this.constituentAtomRepo.delete({ molecule: id })
+    await this.moleculeRepo.delete({ id })
+    return {
+      code: "200",
+      message: "删除成功"
+    }
+  }
+  async allMolecule() {
+
+    const allMolecules = await this.moleculeRepo.find();
+    for (let molecule of allMolecules) {
+      molecule.atomDatas = await this.moleculeRepo.query("select c.*,a.en_name from constituent_atoms c,atom a where c.moleculeId=? and a.id = c.atomId", [molecule.id]);
+    }
+    return {
+      code: "200", data: allMolecules
+    };
+  }
+
+
 
   constructor(@InjectRepository(Molecule)
   private readonly moleculeRepo: Repository<Molecule>,
 
     @InjectRepository(ConstituentAtoms)
-    private readonly atomRepo: Repository<ConstituentAtoms>
+    private readonly constituentAtomRepo: Repository<ConstituentAtoms>
   ) {
 
   }
   async addMolecule(molecule: Molecule) {
-    await this.atomRepo.save(molecule.atomDatas)
+    await this.constituentAtomRepo.save(molecule.atomDatas)
     await this.moleculeRepo.save(molecule)
+    return { code: "200", message: "添加成功" }
   }
 }
